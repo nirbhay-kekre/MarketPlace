b80f13b7bbb9b717708fb4aa666e2f11270b517c
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-12-08 02:20:22 -0800

    adding common header

--- cart.php
+++ cart.php
@@ -1,6 +1,7 @@
 <?php
 require 'auth.php';
-
+include 'rooturl.php';
+$rooturl = getRootURL();
 class Products{
     public $id="";
     public $from="";
@@ -91,51 +92,15 @@ if(isset($_GET['from']) && isset($_GET['id']))
 	<!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
-
+	<meta name="google-signin-client_id" content="853555902851-6pavsf9g93rgo2fgm4gesprkhsf2p0bn.apps.googleusercontent.com">
+	<script src="https://apis.google.com/js/platform.js?onload=onLoad" async defer></script>
 	</head>
 	<body>
 		
 	<div class="colorlib-loader"></div>
 
 	<div id="page">
-		<nav class="colorlib-nav" role="navigation">
-			<div class="top-menu">
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-2">
-							<div id="colorlib-logo"><a href="index.php">Store</a></div>
-						</div>
-						<div class="col-xs-10 text-right menu-1">
-							<ul>
-								<li><a href="index.php">Home</a></li>
-								<li class="has-dropdown">
-									<a href="shop.php?from=all">Shop</a>
-									<ul class="dropdown">
-										<li><a href="product-detail.php?id=1&from=akshay">Product Detail</a></li>
-										<li><a href="cart.php">Shipping Cart</a></li>
-										<li><a href="checkout.php">Checkout</a></li>
-										<li><a href="order-complete.php">Order Complete</a></li>
-										<li><a href="add-to-wishlist.php">Wishlist</a></li>
-									</ul>
-								</li>
-								<li><a href="blog.php">Blog</a></li>
-								<li><a href="about.php">About</a></li>
-								<li><a href="contact.php">Contact</a></li>
-								<li class="active"><a href="cart.php"><i class="icon-shopping-cart"></i> Cart [0]</a></li>
-								<?php 
-									if(isset($_SESSION['SESS_USER_FNAME']))
-									{
-								?>
-								<li><a href="logout.php">Logout <?php echo "{$_SESSION['SESS_USER_FNAME']}"?></a></li>
-								<?php
-									}
-								?>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
+		<?php include 'header.php';?>
 		<aside id="colorlib-hero" class="breadcrumbs">
 			<div class="flexslider">
 				<ul class="slides">
--- rooturl.php
+++ rooturl.php
@@ -1,5 +1,9 @@
 <?php 
     function getRootURL(){
+<<<<<<< Updated upstream
         return "https://sinnonyms.ml/market_place_dev_yash";
+=======
+        return "https://sinnonyms.ml/market_place_dev_nirbhay";
+>>>>>>> Stashed changes
     }
 ?>
