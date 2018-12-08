553255a1ca0d8a226d47fd5c21becd73c40cd9bd
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-12-08 04:06:58 -0800

    updating company title in header

--- header.php
+++ header.php
@@ -56,7 +56,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-2">
-                    <div id="colorlib-logo"><a href=<?php echo '"'.$rooturl.'/index.php"'; ?>>Store</a></div>
+                    <div id="colorlib-logo"><a href=<?php echo '"'.$rooturl.'/index.php"'; ?>>Sinnonyms Inc.</a></div>
                 </div>
                 <div class="col-xs-10 text-right menu-1">
                     <ul>
