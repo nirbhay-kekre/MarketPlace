69bde8633c6c63c59b982015ccfabc9a1164ccba
Nirbhay Kekre <Nirbhay.Kekre@gmail.com>
2018-12-08 02:23:09 -0800

    resolving confilicts

--- rooturl.php
+++ rooturl.php
@@ -1,9 +1,5 @@
 <?php 
     function getRootURL(){
-<<<<<<< Updated upstream
         return "https://sinnonyms.ml/market_place_dev_yash";
-=======
-        return "https://sinnonyms.ml/market_place_dev_nirbhay";
->>>>>>> Stashed changes
     }
 ?>
